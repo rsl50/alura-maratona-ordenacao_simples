aa1385be68f857ae59ca1aaafd58812f0c959a27
Robson Lima <rsl.robson@gmail.com>
2020-01-10 16:46:35 -0300

    Adicionado uso de qsort para ordenção do vetor de entrada

--- ordenacao.c
+++ ordenacao.c
@@ -18,35 +18,46 @@ void selection_sort (long int v[], int size) {
 	}
 }
 
+long int compare (const void *a, const void *b) {
+	if (*(long int*)a > *(long int*)b) {
+		return 1;
+	} else if (*(long int*)a < *(long int*)b) {
+		return -1;
+	} else {
+		return 0;
+	}
+}
+
 int main () {
 	
 	FILE* fp;
-	fp = freopen("input1.txt", "r", stdin);
+	fp = freopen("input3.txt", "r", stdin);
 	
-	char buf[10005];	
-	memset(buf, 0, 10005);
+	char buf[1000009];	
+	memset(buf, 0, 1000009);
 	
 	if (fp != NULL) {
-		fgets(buf, 10005, fp);
+		fgets(buf, 1000009, fp);
 		long int n = strtol(buf, NULL, 10);
 		
 		long int numeros[n];
 		memset(numeros, 0, n);
 		
-		int i;
+		long int i;
 		char *pEnd = NULL;
-		fgets(buf, 10005, fp);
+		fgets(buf, 1000009, fp);
 		for (i = 0; i < n; i++) {
-			numeros[i] = strtol(buf, &pEnd, 0);		
+			numeros[i] = strtol(buf, &pEnd, 10);
 			memcpy(buf, &pEnd[1], strlen(pEnd));
+			fgets(buf, 1000009, fp);
 		}
 		
-		selection_sort(numeros, n);
+		//selection_sort(numeros, n);
+		qsort(numeros, n, sizeof(long int), compare);
 		
 		for (i = 0; i < n; i++) {
 			printf("%d ", numeros[i]);
-		}
-		
+		}		
 	}
 	
 	return (0);
